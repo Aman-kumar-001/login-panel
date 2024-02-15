1ddae18aee0b39505c542a4e0c993475e109aa58
Aman kumar <104027397+Aman-kumar-001@users.noreply.github.com>
2024-02-15 13:56:46 +0530

    till register page

--- src/component/register/register.jsx
+++ src/component/register/register.jsx
@@ -3,8 +3,15 @@ import '../register/register.css';
 
 function register() {
   return (
-    <div>
-       register
+    <div className='register'>
+      <h1>Regiter</h1>
+      <input type='text' placeholder='your Name'></input>
+      <input type='text' placeholder='your Email'></input>
+      <input type='password' placeholder='your Password'></input>
+      <input type='password' placeholder='Re-enter your password'></input>
+      <div className="button">Register</div>
+      <div>or</div>
+      <div className="button">Login</div>
     </div>
   )
 }
