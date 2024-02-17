4c7554c9855af7959f46f9a98a3635d2465393e8
Aman kumar <104027397+Aman-kumar-001@users.noreply.github.com>
2024-02-17 15:41:19 +0530

    till routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import Homepage  from "./component/homepage/homepage";
+import Homepage from "./component/homepage/homepage";
 import Register from "./component/register/register";
 import Login from "./component/login-UI/login";
-import '../src/App.css';
-import { BrowserRouter as Router  , Route} from "react-router-dom";
-import Switch from "react-switch";
+import "../src/App.css";
+import { BrowserRouter ,Routes, Route } from "react-router-dom";
+// import Switch from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-     <Router>
-      <Switch>
-        <Route path="/"><Homepage/></Route>
-        <Route path="/register"><Register/></Route>
-        <Route path="/login"><Login/></Route>
-      </Switch>
-     </Router>
+      <BrowserRouter>
+       <Routes>
+       
+         <Route exact path="/" element={<Homepage />}></Route>
+         <Route  path="/login" element={<Login />}></Route>
+         <Route  path="/register" element={<Register />}></Route>
+      
+       
+       </Routes> 
+       </BrowserRouter>
     </div>
   );
 }
